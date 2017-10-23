e5e8b15a0257c28ecb6be8da3f039de4a1943f95
Pavlo Snylyk <stanova94@gmail.com>
2017-10-23 20:40:28 +0300

    Adding all files on my first commit

---
+++ hello world inernetional/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hello_world_inernetional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] HelloWorld = { "hello world", "Hola Mundo", "Ciao mondo", "Привіт Світ", "Hallo Welt" };
+            for(int i=0;i<HelloWorld.Length;i++)
+            {
+                Console.WriteLine(HelloWorld[i]);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
